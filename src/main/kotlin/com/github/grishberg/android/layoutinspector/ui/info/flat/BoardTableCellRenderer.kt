5bd1db556b8e1737734da945285b933940fd302d
Grigory Rylov <grishberg@yandex-team.ru>
2022-06-17 13:32:29 +0300

    fixed properties panel groups height

--- src/main/kotlin/com/github/grishberg/android/layoutinspector/ui/info/flat/BoardTableCellRenderer.kt
+++ src/main/kotlin/com/github/grishberg/android/layoutinspector/ui/info/flat/BoardTableCellRenderer.kt
@@ -2,12 +2,20 @@ package com.github.grishberg.android.layoutinspector.ui.info.flat
 
 import com.github.grishberg.android.layoutinspector.ui.theme.ThemeColors
 import java.awt.Component
+import java.awt.Insets
 import javax.swing.JTable
+import javax.swing.border.CompoundBorder
+import javax.swing.border.EmptyBorder
 import javax.swing.table.DefaultTableCellRenderer
+
+
+private const val BORDER_SIZE = 4
 
 class BoardTableCellRenderer(
     private val themeColors: ThemeColors
 ) : DefaultTableCellRenderer() {
+
+    private var itemHeight = 0
 
     override fun getTableCellRendererComponent(
         table: JTable, value: Any,
@@ -17,6 +25,9 @@ class BoardTableCellRenderer(
             table, value,
             isSelected, hasFocus, row, col
         )
+        if (itemHeight == 0) {
+            itemHeight = ui.getPreferredSize(this)?.height ?: 36
+        }
 
         val viewRow = table.convertRowIndexToModel(row)
         if (viewRow < 0) {
@@ -27,8 +38,13 @@ class BoardTableCellRenderer(
         when (valueAt) {
             is TableValue.Empty,
             is TableValue.Header -> {
+                border = CompoundBorder(
+                    EmptyBorder(Insets(BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, BORDER_SIZE)),
+                    border
+                )
                 c.foreground = themeColors.groupForeground
                 c.background = themeColors.groupBackground
+                table.setRowHeight(row, itemHeight + BORDER_SIZE * 2)
             }
             is TableValue.PropertyValue,
             is TableValue.PropertyName -> {
