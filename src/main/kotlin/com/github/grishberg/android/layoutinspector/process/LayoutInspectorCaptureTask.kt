7fdd8d72cf029c455cc7bb6bea1c935312b04914
Grirgory Rylov <grishberg@gmail.com>
2023-08-23 10:16:53 +0300

    separate view pumps

--- src/main/kotlin/com/github/grishberg/android/layoutinspector/process/LayoutInspectorCaptureTask.kt
+++ src/main/kotlin/com/github/grishberg/android/layoutinspector/process/LayoutInspectorCaptureTask.kt
@@ -6,6 +6,7 @@ import com.android.layoutinspector.LayoutInspectorCaptureOptions
 import com.android.layoutinspector.LayoutInspectorResult
 import com.android.layoutinspector.ProtocolVersion
 import com.android.layoutinspector.common.AppLogger
+import com.github.grishberg.android.layoutinspector.domain.AbstractViewNode
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -19,7 +20,8 @@ class LayoutInspectorCaptureTask(
 ) {
 
     suspend fun capture(recordingConfig: RecordingConfig): LayoutInspectorResult {
-        val result = scope.async(Dispatchers.IO) {
+
+        val layoutResultAsync = scope.async(Dispatchers.IO) {
             logger.d("$TAG: start capture view timeout = ${recordingConfig.timeoutInSeconds}")
 
             try {
@@ -32,15 +34,6 @@ class LayoutInspectorCaptureTask(
                 val captureView = LayoutInspectorBridge.captureView(
                     logger, recordingConfig.clientWindow, options, recordingConfig.timeoutInSeconds.toLong()
                 )
-                if (recordingConfig.recordOptions.dumpViewModeEnabled) {
-                    val dumper = HierarchyDump(recordingConfig.client.device, layoutFileSystem)
-                    val dumpString = dumper.getHierarchyDump()
-                    dumpString?.let {
-                        val dumpParser = HierarchyDumpParser()
-                        val dumpRootNode = dumpParser.parseDump(dumpString) ?: return@let
-                        return@async LayoutInspectorResult(dumpRootNode, dumpRootNode, captureView.previewImage, captureView.data, captureView.options, captureView.error)
-                    }
-                }
 
                 return@async captureView
             } catch (e: Exception) {
@@ -48,9 +41,36 @@ class LayoutInspectorCaptureTask(
                 throw e
             }
         }
-        val await = result.await()
-        logger.d("$TAG: capturing is done, error: ${await.error}")
-        return await
+        val viewDumpsAsync = scope.async(Dispatchers.IO) {
+            return@async getViewDumps(recordingConfig)
+        }
+
+        val layoutAsyncResult = layoutResultAsync.await()
+        val dumpNodeAsyncResult = viewDumpsAsync.await()
+
+        val result =
+        if (recordingConfig.recordOptions.dumpViewModeEnabled && dumpNodeAsyncResult != null) {
+            LayoutInspectorResult(dumpNodeAsyncResult, dumpNodeAsyncResult, layoutAsyncResult.previewImage, layoutAsyncResult.data, layoutAsyncResult.options, layoutAsyncResult.error)
+        } else {
+            layoutAsyncResult
+        }
+
+        logger.d("$TAG: capturing is done, error: ${result.error}")
+        return result
+    }
+
+    private fun getViewDumps(recordingConfig: RecordingConfig): AbstractViewNode? {
+        if (!recordingConfig.recordOptions.dumpViewModeEnabled) {
+            return null
+        }
+        logger.d("$TAG: start capture view dumps")
+        val dumper = HierarchyDump(recordingConfig.client.device, layoutFileSystem)
+        val dumpString = dumper.getHierarchyDump() ?: return null
+
+        val dumpParser = HierarchyDumpParser()
+        val dumpRootNode = dumpParser.parseDump(dumpString)
+        return dumpRootNode
+
     }
 
     private fun determineProtocolVersion(apiVersion: Int, v2Enabled: Boolean): ProtocolVersion {
